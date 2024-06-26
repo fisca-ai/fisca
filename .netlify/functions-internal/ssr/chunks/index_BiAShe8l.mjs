f42609930b44045e784ac8e4fa2bb34f2cae6c91
Pankaj Kumar <pankaj4u4m@gmail.com>
2024-06-27 01:00:51 +0200

    Make setting cookie correctly. Astro prevents running same event listener once it has been run. So use is:inline tag on the script to prevent astro processing it.

--- .netlify/functions-internal/ssr/chunks/index_BiAShe8l.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./index_D5fb29rF.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
