58622a9d3687d2adbee2178314d71a93d385130c
Pankaj Kumar <pankaj4u4m@gmail.com>
2024-07-22 18:03:43 +0200

    updater seo metadata tags.

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,7 +2,7 @@ import { ui, defaultLang, showDefaultLang, routes } from "@/i18n/ui";
 
 export function useTranslations(lang: string) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
+    return ui[lang][key] || ui[defaultLang][key] || `${key}`;
   };
 }
 
