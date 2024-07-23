8e74880d021ccfdeacf0a97e15a91674100c62b2
Pankaj Kumar <pankaj4u4m@gmail.com>
2024-07-23 16:37:50 +0200

    fix z index of lang dropdown and translate copywrite

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -34,7 +34,7 @@ export const ui: Ui = {
     "Menu Open": "Menu Open",
     "Menu Close": "Menu Close",
     FAQ: "FAQ",
-    copyright: "© 2022. Fisca. All rights reserved",
+    copyright: "© 2024. Fisca. All rights reserved",
     fisca_title: "Fisca - AI Medical Coding and Billing Platform",
     fisca_description:
       "Empowering healthcare providers with AI-driven diagnostic coding, billing, claim management, and analytics for advanced revenue management and insightful claims processing for a brighter future.",
@@ -59,7 +59,7 @@ export const ui: Ui = {
     "Send Now": "Jetzt senden",
     "Menu Open": "Menu öffnen",
     "Menu Close": "Menu schließen",
-    copyright: "© 2022. Fisca. Alle Rechte vorbehalten",
+    copyright: "© 2024. Fisca. Alle Rechte vorbehalten",
     fisca_title: "Fisca - KI Medizinische Kodierungs- und Abrechnungsplattform",
     fisca_description:
       "Stärkung der Gesundheitsdienstleister durch KI-gestützte diagnostische Kodierung, Abrechnung, Anspruchsmanagement sowie Analysen und Erkenntnisse für eine fortschrittliche Einnahmenverwaltung und optimierte Anspruchsverarbeitung.",
@@ -84,7 +84,7 @@ export const ui: Ui = {
     "Send Now": "Envoyer maintenant",
     "Menu Open": "Menu ouvert",
     "Menu Close": "Menu ferme",
-    copyright: "© 2022. Fisca. Tous droits réservés",
+    copyright: "© 2024. Fisca. Tous droits réservés",
     fisca_title:
       "Fisca - Plateforme de codage et de facturation médicale alimentée par l'IA",
     fisca_description:
@@ -110,7 +110,7 @@ export const ui: Ui = {
     "Send Now": "Invia ora",
     "Menu Open": "Menu aperto",
     "Menu Close": "Menu chiuso",
-    copyright: "© 2022. Fisca. Tutti i diritti riservati",
+    copyright: "© 2024. Fisca. Tutti i diritti riservati",
     fisca_title:
       "Fisca - Piattaforma di codifica e fatturazione medica basata su IA",
     fisca_description:
